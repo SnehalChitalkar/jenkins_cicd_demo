ffc7fa3a579251689f6d5513dd4ae1f7be806686
SnehalChitalkar <snehalchitalkar99@gmail.com>
2026-06-06 21:47:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <h1>Hello, this is a demo project to deploy in CI/CD using Jenkins!</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
